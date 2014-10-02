1ba5037f83cc36766e7e6dfe88bceadd0edb4ce8
Lixquid <lixquid@users.noreply.github.com>
2014-10-02 13:33:00 +0100

    Added Number rules, added test document

---
+++ test.lua
@@ -0,0 +1,14 @@
+-- This document provides a test environment for the Lua grammar.
+-- It is not meant to be run!
+
+-- Numbers -----------------------------------------------------------------------------------------
+
+1
+2e2
+3e-3
+4.4
+5.5e5
+6.6e-6
+.7
+.8e8
+0xa
